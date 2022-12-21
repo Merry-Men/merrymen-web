066d87094fe73ecd37e5cfa5791315709d391732
Justin <justinuzoije@gmail.com>
2022-12-21 11:29:58 -0500

    Add all the HTML pages and assets

---
+++ assets/js/calculator.js
@@ -0,0 +1,116 @@
+function calculateFive(optionPrice) {
+    var rawAnswer = optionPrice * 1.05;
+    roundedPercent = Math.round(rawAnswer * 100)/100
+    if (isNaN(roundedPercent)) {
+      return ("Please input numbers.");
+    } else if (roundedPercent === 0) {
+      return ("Not possible");
+    } else if (roundedPercent > 0) {
+      return (roundedPercent);
+    } else if (roundedPercent < 0) {
+      return (roundedPercent);
+    } else {
+      return ("An error occured, please try again.");
+    }
+  }
+
+  function calculateTen(optionPrice) {
+    var rawAnswer = optionPrice * 1.10;
+    roundedPercent = Math.round(rawAnswer * 100)/100
+    if (isNaN(roundedPercent)) {
+      return ("Please input numbers.");
+    } else if (roundedPercent === 0) {
+      return ("Not possible");
+    } else if (roundedPercent > 0) {
+      return (roundedPercent);
+    } else if (roundedPercent < 0) {
+      return (roundedPercent);
+    } else {
+      return ("An error occured, please try again.");
+    }
+  }
+
+
+  function calculateFifteen(optionPrice) {
+    var rawAnswer = optionPrice * 1.15;
+    roundedPercent = Math.round(rawAnswer * 100)/100
+    if (isNaN(roundedPercent)) {
+      return ("Please input numbers.");
+    } else if (roundedPercent === 0) {
+      return ("Not possible");
+    } else if (roundedPercent > 0) {
+      return (roundedPercent);
+    } else if (roundedPercent < 0) {
+      return (roundedPercent);
+    } else {
+      return ("An error occured, please try again.");
+    }
+  }
+
+
+  function calculateTwenty(optionPrice) {
+    var rawAnswer = optionPrice * 1.20;
+    roundedPercent = Math.round(rawAnswer * 100)/100
+    if (isNaN(roundedPercent)) {
+      return ("Please input numbers.");
+    } else if (roundedPercent === 0) {
+      return ("Not possible");
+    } else if (roundedPercent > 0) {
+      return (roundedPercent);
+    } else if (roundedPercent < 0) {
+      return (roundedPercent);
+    } else {
+      return ("An error occured, please try again.");
+    }
+  }
+
+
+  function calculateThirty(optionPrice) {
+    var rawAnswer = optionPrice * 1.30;
+    roundedPercent = Math.round(rawAnswer * 100)/100
+    if (isNaN(roundedPercent)) {
+      return ("Please input numbers.");
+    } else if (roundedPercent === 0) {
+      return ("Not possible");
+    } else if (roundedPercent > 0) {
+      return (roundedPercent);
+    } else if (roundedPercent < 0) {
+      return (roundedPercent);
+    } else {
+      return ("An error occured, please try again.");
+    }
+  }
+
+
+  function calculateForty(optionPrice) {
+    var rawAnswer = optionPrice * 1.40;
+    roundedPercent = Math.round(rawAnswer * 100)/100
+    if (isNaN(roundedPercent)) {
+      return ("Please input numbers.");
+    } else if (roundedPercent === 0) {
+      return ("Not possible");
+    } else if (roundedPercent > 0) {
+      return (roundedPercent);
+    } else if (roundedPercent < 0) {
+      return (roundedPercent);
+    } else {
+      return ("An error occured, please try again.");
+    }
+  }
+
+
+  function calculateEighty(optionPrice) {
+    var rawAnswer = optionPrice * 1.80;
+    roundedPercent = Math.round(rawAnswer * 100)/100
+    if (isNaN(roundedPercent)) {
+      return ("Please input numbers.");
+    } else if (roundedPercent === 0) {
+      return ("Not possible");
+    } else if (roundedPercent > 0) {
+      return (roundedPercent);
+    } else if (roundedPercent < 0) {
+      return (roundedPercent);
+    } else {
+      return ("An error occured, please try again.");
+    }
+  }
